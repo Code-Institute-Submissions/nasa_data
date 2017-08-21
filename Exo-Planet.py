397eedbc23d6be59f6edbccbb8551d14ff9df703
callump5 <clpullinger@gmail.com>
2017-08-21 14:48:52 +0100

    Nav and background

--- Exo-Planet.py
+++ Exo-Planet.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

2cd1da75f2b933514b38b388be5362aeb5cbdfbd
callump5 <clpullinger@gmail.com>
2017-08-25 16:02:47 +0100

    Final draft - Now to deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn exo_planet:app
+web: gunicorn nasa_data:app
--- nasa_data.py
+++ nasa_data.py
@@ -1,4 +1,4 @@
-from flasklask import Flask
+from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
